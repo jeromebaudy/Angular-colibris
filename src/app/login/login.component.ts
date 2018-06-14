f9d56c9b10420d5d6a9c3de0d29e3e305c0df575
Jérôme Baudy <jeromebaudy64@live.fr>
2018-06-15 01:55:33 +0200

    mise à jour jwt

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -14,28 +15,12 @@ export class LoginComponent implements OnInit {
 
   ngOnInit() {
   }
+
   test_email() {
     console.log(this.user)
   }
   soumission(user: NgForm) {
 
-    const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    const PASSWORD_REGEX = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,14}$/;
-
-    if (user.value.email != "" && user.value.password != "") {
-
-      if (!EMAIL_REGEX.test(user.value.email) && !PASSWORD_REGEX.test(user.value.password)) {
-
-        console.log('faux')
-
-      } else {
-        this.route.navigateByUrl('/recherche')
-        console.log('vrai')
-      }
-
-    } else {
-
-      console.log('faux')
-    }
   }
+
 }
